8b8ce1dd8536ae5eed50f1a51d704cec7f8763c9
Nick Gauthier <nick@smartlogicsolutions.com>
2011-03-16 13:22:40 -0400

    client-side twitter call

--- src/public/bitter.js
+++ src/public/bitter.js
@@ -1,12 +1,26 @@
+Backbone._sync = Backbone.sync;
+Backbone.sync = function(method, model, success, error) {
+  if (model.url.indexOf('http://api.twitter.com') === 0) {
+    $.ajax({
+      url: model.url,
+      type: 'get',
+      contentType: 'application/json',
+      dataType: 'jsonp',
+      success: success,
+      error: error
+    });
+  } else {
+    Backbone._sync(method, model, success, error);
+  }
+}
+
+
 var Tweet = Backbone.Model.extend({
 
 });
 var Tweets = Backbone.Collection.extend({
   model: Tweet,
-  url: '/tweets', 
-  initialize: function(options) {
-    this.screen_name = options.screen_name;
-  }
+  url: "http://api.twitter.com/1/statuses/public_timeline.json"
 });
 
 var TweetView = Backbone.View.extend({
@@ -34,7 +48,7 @@ var TweetList = new (Backbone.View.extend({
   }
 }))();
 
-var Feed = new Tweets({screen_name: 'ngauthier'});
+var Feed = new Tweets();
 
 Feed.bind('refresh', TweetList.render);
 //Feed.bind('refresh', function() { TweetList.render()});
